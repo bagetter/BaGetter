9050bbe95788a21eb5d55bcf836a1f78afad2332
Marlon Regenhardt <marlon.r@hotmail.de>
2024-02-12 21:19:28 +0100

    Switch to using declarations

--- src/BaGetter.Core/Extensions/StreamExtensions.cs
+++ src/BaGetter.Core/Extensions/StreamExtensions.cs
@@ -47,13 +47,11 @@ namespace BaGetter.Core
 
         public static bool Matches(this Stream content, Stream target)
         {
-            using (var sha256 = SHA256.Create())
-            {
-                var contentHash = sha256.ComputeHash(content);
-                var targetHash = sha256.ComputeHash(target);
+            using var sha256 = SHA256.Create();
+            var contentHash = sha256.ComputeHash(content);
+            var targetHash = sha256.ComputeHash(target);
 
-                return contentHash.SequenceEqual(targetHash);
-            }
+            return contentHash.SequenceEqual(targetHash);
         }
     }
 }
