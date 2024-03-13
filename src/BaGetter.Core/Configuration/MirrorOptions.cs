4a6ad2e1b1a744f6c19b995b291cc731032807e7
Splamy <splamyn@gmail.com>
2024-03-13 18:56:44 +0100

    Split mirror options file into file for each class

--- src/BaGetter.Core/Configuration/MirrorOptions.cs
+++ src/BaGetter.Core/Configuration/MirrorOptions.cs
@@ -90,24 +90,3 @@ public class MirrorOptions : IValidatableObject
         }
     }
 }
-
-public class MirrorAuthenticationOptions
-{
-    public MirrorAuthenticationType Type { get; set; } = MirrorAuthenticationType.None;
-
-    public string Username { get; set; }
-
-    public string Password { get; set; }
-
-    public string Token { get; set; }
-
-    public Dictionary<string, string> CustomHeaders { get; set; } = [];
-}
-
-public enum MirrorAuthenticationType
-{
-    None,
-    Basic,
-    Bearer,
-    Custom
-}
